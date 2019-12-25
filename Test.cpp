649eefb8b2c86fb1e5f065383e5b19c018b1727e
xdobetter <15170505655@163.com>
2019-12-25 20:22:11 +0800

    添加项目文件。

---
+++ Test.cpp
@@ -0,0 +1,30 @@
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ *                                                                                                 
+ * Blog: https://blog.csdn.net/weixin_41234001                                      
+ *                                                                                                 
+ * Author: DoBetter                                                               
+ *                                                                                                 
+ * Time: 2019.12.25                                                                            
+ *                                                                                                 
+ * Describe: 第一个OpenGL程序                                             
+ *                                                                                                 
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ */
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "HelloWorld", NULL, NULL);
+	//创建窗口 
+	system("pause");
+	return 0;
+}
